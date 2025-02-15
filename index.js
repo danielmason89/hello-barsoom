9ca0e304234443c15271c9709315993cd3f5e422
Daniel Mason <danmasson0@gmail.com>
2025-02-15 20:36:19 +0000

    new version, small change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 
 try {
-    const name = core.getInput("who-to-greet");
+    const name = core.getInput("name");
     const output_value = `Hello ${name}!`;
     core.setOutput("greeting", output_value);
 } catch (error) {
